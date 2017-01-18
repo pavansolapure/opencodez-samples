e77bd4821fa3326b38ffcdac6c2ff6f7a634f062
Pavan Solapure <pavan.solapure@gmail.com>
2017-01-18 16:43:33 +0530

    Initial Commit

---
+++ generic-csv-reader/src/main/java/com/opencodez/GenericCsvReaderApplication.java
@@ -0,0 +1,12 @@
+package com.opencodez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericCsvReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericCsvReaderApplication.class, args);
+	}
+}
