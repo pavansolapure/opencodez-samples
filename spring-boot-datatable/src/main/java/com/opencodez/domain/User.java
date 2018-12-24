d8c7878bec3bc22d20325d62d4354e1c74a8268e
Sahici, Kivanc <kivancsahici@yahoo.com>
2018-12-24 13:41:32 +0300

    mark totalRecords as Transient

--- spring-boot-datatable/src/main/java/com/opencodez/domain/User.java
+++ spring-boot-datatable/src/main/java/com/opencodez/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  * @author pavan.solapure
@@ -26,6 +27,7 @@ public class User {
 	@Column(name = "SALARY")
 	private Integer salary;
 	
+	@Transient
 	private Integer totalRecords;
 	
 	/**
